ac1ce7d7baaf869d1acd9933a7a1a09335b3436c
StPetersJLN <99016824+StPetersJLN@users.noreply.github.com>
2022-02-04 11:41:49 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.splash("splash")
+})
 let mySprite = sprites.create(assets.image`bout`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.setBackgroundColor(6)
+scene.cameraFollowSprite(mySprite)
